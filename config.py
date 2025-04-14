ccbf09feb5b912041026523d1541bd3eaa3f895a
Ehsan <144959137+TssHack@users.noreply.github.com>
2025-04-14 08:44:39 +0330

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from dotenv import load_dotenv
 load_dotenv() # Loads variables from .env file if it exists
 
 BOT_TOKEN = os.getenv("BOT_TOKEN", "1752263879:AR7EWOyRTpIcTXyQG7kq3ZbHFBaAyFV43rEC8krO") # توکن ربات خود را اینجا یا در فایل .env قرار دهید
-ADMIN_USER_IDS = [int(admin_id) for admin_id in os.getenv("ADMIN_USER_IDS", "YOUR_ADMIN_USER_ID").split(',')] # لیست ایدی عددی ادمین‌ها (جدا شده با کاما)
+ADMIN_USER_IDS = [int(admin_id) for admin_id in os.getenv("ADMIN_USER_IDS", "2143480267").split(',')] # لیست ایدی عددی ادمین‌ها (جدا شده با کاما)
 # مثال: ADMIN_USER_IDS = "12345678,98765432"
 
 DATABASE_NAME = "bot_database.db"
